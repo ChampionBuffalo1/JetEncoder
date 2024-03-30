cd8d9796c91a7921603837c7ea74e8759cb3207f
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-30 18:26:01 +0530

    feat: Storage clients

---
+++ src/storages.ts
@@ -0,0 +1,19 @@
+import { S3Client } from '@aws-sdk/client-s3';
+
+const uploadStorage = new S3Client({
+  region: 'auto',
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
+  }
+});
+// TODO: Generate a GET Presigned URL from S3 Lambda instead
+const downloadStorage = new S3Client({
+  region: 'ap-south-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+export { downloadStorage, uploadStorage };
